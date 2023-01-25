2a361559655f08070b21448816c3d2a61a584623
Michael Woo <michaelwoo36@gmail.com>
2023-01-24 23:05:20 -0800

    Final

--- Deck.java
+++ Deck.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Deck {
+
     public  ArrayList<String[]> deck;
     private String suit;
     public Stack<String[]> deal;
@@ -13,23 +14,26 @@ public class Deck {
     private Scanner input;
 
     public Deck(){
+
         deal = new Stack<>();
         input = new Scanner(System.in);
         playerHand = new ArrayList<>();
         dealerHand = new ArrayList<>();
-
     }
 
     // deals first hand to dealer and player.
     public void startGame() throws InterruptedException  {
+
         System.out.println("Dealers hand:");
         Thread.sleep(750);
         dealerHand.add(deal.pop());
         dealerHand.add(deal.pop());
 
         for (String[] object : dealerHand) {
+
             dealerTotal += dealerAddPoints(object);
         }
+
         System.out.println("Hole card");
         System.out.println(toString(dealerHand.get(1)));
         System.out.println();
@@ -38,10 +42,14 @@ public class Deck {
         System.out.println("Your Hand:");
         playerHand.add(deal.pop());
         playerHand.add(deal.pop());
+
         for (String[] object : playerHand) {
+
             System.out.println(toString(object));
         }
+
         for (String[] object : playerHand) {
+
             playerTotal += addPoints(object);
         }
 
@@ -51,6 +59,7 @@ public class Deck {
 
     // checks if player has blackjack and tells who will win, lose, or tie
     public void checkBlackJack() throws InterruptedException {
+
         System.out.println();
         System.out.println("Black Jack!");
         Thread.sleep(750);
@@ -60,11 +69,15 @@ public class Deck {
         System.out.println(toString(dealerHand.get(1)));
         System.out.println("Dealer Total: " + dealerTotal);
         Thread.sleep(750);
+
         if (dealerTotal != playerTotal) {
+
             System.out.println();
             System.out.println("congrats you win!");
         }
+
         else {
+
             System.out.println();
             System.out.println("Push!");
         }
@@ -72,56 +85,86 @@ public class Deck {
 
     // until 21 is reached the player will be asked to stand or hit
     public void standOrHit() throws InterruptedException {
+
         while(playerTotal < 21) {
+
             System.out.println();
             System.out.println("Stand or Hit (s/h)");
             // Stand procedure.
+
             if (input.next().equals("s")) {
+
                 while (dealerTotal < 17) {
+
                     dealerHand.add(deal.pop());
                     dealerTotal += dealerAddPoints(dealerHand.get(dealerHand.size() - 1));
                 }
+
                 System.out.println();
                 Thread.sleep(750);
                 System.out.println("Dealers Hand");
+
                 for (String[] object : dealerHand) {
+
                     System.out.println(toString(object));
                 }
+
                 System.out.println("Dealer Total: " + dealerTotal);
                 System.out.println();
                 Thread.sleep(750);
                 System.out.println("Your Hand");
+
                 for (String[] object : playerHand) {
+
                     System.out.println(toString(object));
                 }
+
                 System.out.println("Player total: " + playerTotal);
                 Thread.sleep(750);
+
                 if (dealerTotal <= 21) {
+
                     if (playerTotal == dealerTotal) {
+
                         System.out.println();
                         System.out.println("push");
-                    } else if (dealerTotal > playerTotal) {
+                    }
+
+                    else if (dealerTotal > playerTotal) {
+
                         System.out.println();
                         System.out.println("Dealer wins");
                     }
+
                     else {
+
                         System.out.println();
                         System.out.println("You win.");
                     }
+
                     return;
-                } else if (dealerTotal > 21) {
+                }
+
+                else if (dealerTotal > 21) {
+
                     System.out.println();
                     System.out.println("dealer busts");
                     System.out.println();
                     System.out.println("you win!");
+
                     return;
                 }
+
                 else {
+
                     if (dealerTotal > playerTotal) {
+
                         System.out.println();
                         System.out.println("dealer Wins!");
                     }
+
                     else {
+
                         System.out.println();
                         System.out.println("You win");
                     }
@@ -130,33 +173,53 @@ public class Deck {
 
             // Hit
             else {
+
                 playerHand.add(deal.pop());
                 System.out.println();
                 System.out.println("Your Hand");
+
                 for (String[] object : playerHand) {
+
                     System.out.println(toString(object));
                 }
+
                 playerTotal += addPoints(playerHand.get(playerHand.size() - 1));
                 System.out.println("Player total: " + playerTotal);
                 Thread.sleep(750);
+
                 if (playerTotal == 21) {
+                    while (dealerTotal < 17) {
+
+                        dealerHand.add(deal.pop());
+                        dealerTotal += dealerAddPoints(dealerHand.get(dealerHand.size() - 1));
+                    }
                     System.out.println();
                     System.out.println("Dealers Hand:");
+
                     for (String[] object : dealerHand) {
+
                         System.out.println(toString(object));
                     }
+
                     System.out.println("Dealer total:" + dealerTotal);
                     Thread.sleep(750);
+
                     if (dealerTotal != playerTotal) {
+
                         System.out.println();
                         System.out.println("you win!");
                     }
+
                     else {
+
                         System.out.println();
                         System.out.println("Dealers Hand:");
+
                         for (String[] object : dealerHand) {
+
                             System.out.println(toString(object));
                         }
+
                         System.out.println("Dealer total:" + dealerTotal);
                         System.out.println();
                         Thread.sleep(750);
@@ -164,7 +227,9 @@ public class Deck {
                     }
 
                 }
+
                 else if (playerTotal > 21) {
+
                     System.out.println();
                     System.out.println("You bust");
 
@@ -174,18 +239,26 @@ public class Deck {
     }
 
     public int getPlayerTotal(){
+
         return playerTotal;
     }
 
     // adds point value of card to hand total for player.
     public int addPoints(String[]Cards){
+
         switch(Cards[1]){
+
             case "Ace":
+
                 System.out.println("choose 1 or 11");
+
                 if(input.next().equals("1")) {
+
                     return 1;
                 }
+
                 else {
+
                     return 11;
                 }
             case "2":
@@ -205,20 +278,30 @@ public class Deck {
             case "9":
                 return 9;
         }
+
         return 10;
     }
 
     // adds point value of card to dealer.
     public int dealerAddPoints(String[]Cards){
+
         switch(Cards[1]){
+
             case "Ace":
+
                 if(dealerHand.size()<3){
+
                     return 11;
                 }
+
                 else {
+
                     if ((dealerTotal + 11) >= 17) {
+
                         return 11;
-                    } else {
+                    }
+                    else {
+
                         return 1;
                     }
                 }
@@ -240,14 +323,18 @@ public class Deck {
             case "9":
                 return 9;
         }
+
         return 10;
     }
 
     //Returns Stack. First makes a shuffled ArrayList, then adds each element into stack.
     public Stack makeDeck (){
+
         deck = new ArrayList<String[]>();
         //adding faces for each suit to arrayList
+
         for(int i = 1; i<=4; i++) {
+
             if(i==1) {
                 suit = "diamonds";
             }
@@ -260,26 +347,34 @@ public class Deck {
             else if(i == 4){
                 suit = "clubs";
             }
+
             deck.add(new Card(suit, "Ace").getCard());
+
             for(int k = 2; k<=10; k++){
+
                 String face = k + "";
                 deck.add(new Card(suit, face).getCard());
             }
+
             deck.add(new Card(suit, "Jack").getCard());
             deck.add(new Card(suit, "Queen").getCard());
             deck.add(new Card(suit, "King").getCard());
         }
+
         //shuffles
         Collections.shuffle(deck);
         //puts elements in stack
+
         for(String[]cards : deck){
             deal.push(cards);
         }
+
         return deal;
     }
 
     //Prints card out as String
     public String toString(String[]cards){
+
         return cards[1] + " of " + cards[0];
     }
 }
--- Main.java
+++ Main.java
@@ -13,21 +13,27 @@ public class Main {
 
         // Plays one game of black jack. Asks to play again.
         while(doYouWantToPlay) {
+
             Deck myDeck = new Deck();
             // makes shuffled deck.
             myDeck.makeDeck();
             myDeck.startGame();
+
             // checks for blackjack on first hand.
             if(myDeck.getPlayerTotal() == 21){
                 myDeck.checkBlackJack();
             }
+
             else {
+
                 // No blackjack means continuing the game to the stand or hit phase
                 myDeck.standOrHit();
                 System.out.println();
             }
+
             System.out.println("Play again? y/n");
             System.out.println();
+
             if(input.next().equals("n")){
                 doYouWantToPlay = false;
             }
